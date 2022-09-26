2517e14b8532458f536c4b1fdae66bbb8eb6e2a3
ap3334 <chwil2989@naver.com>
2022-09-26 23:46:40 +0900

    use builder to create

--- src/test/java/com/example/qsl/user/repository/UserRepositoryTest.java
+++ src/test/java/com/example/qsl/user/repository/UserRepositoryTest.java
@@ -20,8 +20,17 @@ class UserRepositoryTest {
     @DisplayName("회원 2명 생성")
     void t1() {
 
-        SiteUser u1 = new SiteUser(null, "user1", "{noop}1234", "user1@test.com");
-        SiteUser u2 = new SiteUser(null, "user2", "{noop}1234", "user2@test.com");
+        SiteUser u1 = SiteUser.builder()
+                .username("user1")
+                .password("{noop}1234")
+                .email("user1@test.com")
+                .build();
+
+        SiteUser u2 = SiteUser.builder()
+                .username("user2")
+                .password("{noop}1234")
+                .email("user2@test.com")
+                .build();
 
         userRepository.saveAll(Arrays.asList(u1, u2));
     }
