d4b4ce8ba250808dd63ee2cc855faf8aa220e5ed
ap3334 <chwil2989@naver.com>
2022-09-27 09:30:11 +0900

    Add testInitData

---
+++ src/main/java/com/example/qsl/user/base/DevInitData.java
@@ -0,0 +1,7 @@
+package com.example.qsl.user.base;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+public class DevInitData {
+}
