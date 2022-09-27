4d644f049493291fd83645599823726831316fe2
ap3334 <chwil2989@naver.com>
2022-09-27 09:38:14 +0900

    Add @Transactional in test class

--- src/test/java/com/example/qsl/user/repository/UserRepositoryTest.java
+++ src/test/java/com/example/qsl/user/repository/UserRepositoryTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@Transactional
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
